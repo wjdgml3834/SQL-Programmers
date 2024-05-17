c87fdf905e56996c46d6c643b07e7d529b5f9763
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2024-05-17 12:37:08 +0900

    [level 2] Title: 월별 잡은 물고기 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/293260. 월별 잡은 물고기 수 구하기/월별 잡은 물고기 수 구하기.sql
@@ -0,0 +1,18 @@
+/*
+월별 잡은 물고기의 수와 월을 출력하는 SQL문을 작성해주세요.
+잡은 물고기 수 컬럼명은 FISH_COUNT, 월 컬럼명은 MONTH로 해주세요.
+결과는 월을 기준으로 오름차순 정렬해주세요.
+단, 월은 숫자형태 (1~12) 로 출력하며 9 이하의 숫자는 두 자리로 출력하지 않습니다. 잡은 물고기가 없는 월은 출력하지 않습니다.
+*/
+SELECT
+  COUNT(MONTH) AS FISH_COUNT,
+  MONTH
+FROM(
+  SELECT
+    MONTH(TIME) AS MONTH
+  FROM FISH_INFO
+) AS fish_count_month
+GROUP BY
+  MONTH
+ORDER BY
+ MONTH ASC;
